4346f5b75924dc5f6b7a661184b6cf5db841f902
zeredogo <zeredogo@gmail.com>
2022-08-08 22:03:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+function App() {
+  return (
+    <>
+      <h2>navbar project setup</h2>
+    </>
+  )
+}
+
+export default App
